3610f3c7864a0dcb4e7c2c1742c7c32beb2442cf
Mila <Pod4asova20@gmail.com>
2022-04-04 00:56:53 +0300

    Initial commit

---
+++ src/main/java/lesson5/AppData.java
@@ -0,0 +1,24 @@
+package lesson5;
+
+import java.io.Serializable;
+import java.util.Arrays;
+
+public class AppData implements Serializable{
+    private String header;
+    private String data;
+    private String data2;
+
+
+    public AppData(String header, String data, String data2) {
+        this.header = header;
+        this.data = data;
+        this.data2 = data2;
+    }
+
+    @Override
+    public String toString() {
+        return header + ";" +
+                data + ";" +
+                data2;
+    }
+}
